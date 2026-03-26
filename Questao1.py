73f50a977a59254acabbebcbbe66981a5fc14712
Paola Peres <paoladuarte540@gmail.com>
2026-03-26 18:56:18 -0300

    Add files via upload

---
+++ Questao1.py
@@ -0,0 +1,11 @@
+with open("dna.txt", "r") as f:
+    cadeia = f.read().strip()
+
+cadeia_inversa = cadeia[::-1]
+
+with open("dna_resultado.txt", "w") as f:
+    f.write(f"Entrada: {cadeia}\n")
+    f.write(f"Saída: {cadeia_inversa}\n")
+
+print("Entrada:", cadeia)
+print("Saída:", cadeia_inversa)
